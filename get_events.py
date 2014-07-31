dcca9cffd3b4efb44368fc0e5e3970b48100e6df
rezalution <rezalution786@gmail.com>
2014-07-30 22:41:38 -0700

    sim

--- get_events.py
+++
@@ -1,9 +0,0 @@
-page_token = None
-while True:
-  events = service.events().list(calendarId='cs419.team4@gmail.com', pageToken=page_token).execute()
-  if events['items']:
-    for event in events['items']:
-      print event['summary']
-  page_token = events.get('nextPageToken')
-  if not page_token:
-    breaks
